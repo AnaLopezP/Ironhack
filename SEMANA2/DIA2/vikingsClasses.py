728bda05de1a4d47f14b3020bc24228b51e53e4b
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-24 11:48:05 +0200

    status

--- SEMANA2/DIA2/vikingsClasses.py
+++ SEMANA2/DIA2/vikingsClasses.py
@@ -74,3 +74,11 @@ class War:
         if Soldier.health == 0:
             self.saxonArmy.pop()
         return Viking.receiveDamage(self, Saxon.attack(self))
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
