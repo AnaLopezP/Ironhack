f74c5ecde437d0b8dafb6416ffb64d21b5c0399c
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-24 12:37:33 +0200

    :(

--- SEMANA2/DIA2/vikingsClasses.py
+++ SEMANA2/DIA2/vikingsClasses.py
@@ -69,7 +69,7 @@ class War:
         s.receiveDamage(v.strength)
         if s.health == 0:
             self.saxonArmy.remove(s)
-            return s.health
+            return print(s.receiveDamage(v.strength))
         else:
             pass
 
@@ -79,7 +79,7 @@ class War:
         v.receiveDamage(s.strength)
         if v.health == 0:
             self.vikingArmy.remove(v)
-            return v.health
+            return print(v.receiveDamage(s.strength))
         else:
             pass
 
