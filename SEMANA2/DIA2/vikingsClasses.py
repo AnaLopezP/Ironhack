e36c782526ef1eac1b470e7064c06e95fe62d074
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-24 10:46:00 +0200

    copiar los enunciados

---
+++ SEMANA2/DIA2/vikingsClasses.py
@@ -0,0 +1,37 @@
+
+# Soldier
+
+
+class Soldier:
+    
+    def __init__(self, health, strength):
+        
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        
+        self.health = self.health - damage
+
+# Viking
+
+
+class Viking(Soldier):
+    pass
+    
+
+# Saxon
+
+
+class Saxon:
+    pass
+
+# War
+
+
+class War:
+    pass
