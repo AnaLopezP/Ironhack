ee301c8104a5b772272d14d29436dc69340cb24c
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-24 11:31:45 +0200

    aaaaaaaaaa

--- SEMANA2/DIA2/vikingsClasses.py
+++ SEMANA2/DIA2/vikingsClasses.py
@@ -54,4 +54,17 @@ class Saxon(Soldier):
 
 
 class War:
-    pass
+    def __init__(self, vikingArmy = [], saxonArmy = []):
+        self.vikingArmy = vikingArmy
+        self.saxonArmy = saxonArmy
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+    
+    def vikingAttack(self):
+        Saxon.receiveDamage(self, Viking.strength)
+        if Saxon.health == 0:
+            self.saxonArmy.pop(Saxon)
